211c62797673cb479cf6a665c2645b26b66ed9e6
cfriedrich <chrisfriedrich@outlook.com>
2014-04-07 15:11:47 -0700

    UserChange
    Changed User back to Member

--- EquipmentManager/EquipmentManager.WebUI/Controllers/AccountController.cs
+++ EquipmentManager/EquipmentManager.WebUI/Controllers/AccountController.cs
@@ -17,8 +17,8 @@ namespace EquipmentManager.WebUI.Controllers
     {
         private  EFMemberRepository memRepo = new EFMemberRepository();
 
-        private const String MEMBERNAME = "username";
-        private const String MEMBER = "user";
+        private const String MEMBERNAME = "membername";
+        private const String MEMBER = "member";
 
         public AccountController(EFMemberRepository memberRepository)
         {
